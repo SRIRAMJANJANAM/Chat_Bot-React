58e65004908a165a457af4d3143206d0bfbf92cb
sriram <sriramjanjanamram@gmail.com>
2025-08-21 15:12:43 +0530

    updated

--- src/api.js
+++ src/api.js
@@ -1,2 +1,5 @@
 import axios from 'axios'
-export const API = axios.create({ baseURL: 'http://127.0.0.1:8000/api' })
+export const API = axios.create({
+  baseURL: 'http://172.26.0.117:8000/api'
+});
+
